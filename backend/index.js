c82ab6daebbd074674480fe3400b66c70190fdf8
Pranav Kethan <pranavkethan@gmail.com>
2025-05-08 09:56:25 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
+const mongodbURL=process.env.mongodbURL;
+mongoose.connect(mongodbURL)
+.then(()=>{console.log("connected to mongoDB")})
+.catch((err)=>{console.log("mongodb connection error",err.message)})
+
+const app=express();
+
+app.use(express.json())
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running in port : ${process.env.PORT}`)
+})
